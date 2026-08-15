f61da14754e6c8775b14c7a2e6a1a2cd0b0cdaca
Sai Buthada <94958989+BsdSaiPrasad@users.noreply.github.com>
2026-08-15 14:33:56 -0400

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,16 @@
+# TC: O(n log n) — sorting dominates; one pass after that is O(n). SC: O(n) — pairs + stack can store up to n cars/fleets.
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        stack = [] # to maintain the current maximum seen so far that is the slowest fleet time on top
+        pairs = [(p,s) for p,s in zip(position, speed)]
+        pairs.sort(reverse = True)
+        for p,s in pairs:
+            time = (target - p)/ s
+            if not stack or time > stack[-1]:
+                stack.append(time)
+        return len(stack)
+
+        
+
+
+        
