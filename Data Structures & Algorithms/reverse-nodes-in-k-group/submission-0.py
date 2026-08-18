16ffcdfa883a1683a9a4f3449ff137a1ff14e2a2
Sai Buthada <94958989+BsdSaiPrasad@users.noreply.github.com>
2026-08-18 01:47:24 -0400

    Add: reverse-nodes-in-k-group - submission-0

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-0.py
@@ -0,0 +1,53 @@
+# TC: O(n)  
+# Every node is visited once to:find k-th node
+# reverse pointers
+# reconnect groups
+
+# SC: O(1)  
+# Only uses pointers:prev
+# curr
+# next_node
+# kth
+# group_prev
+
+# No extra list/stack created.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+
+        dummy = ListNode(0)
+        dummy.next = head
+        group_prev = dummy
+
+        while True:
+
+            kth = group_prev
+
+            for i in range(k):
+                kth = kth.next
+                if kth is None:
+                    return dummy.next
+
+            group_next = kth.next
+
+            # reverse k nodes
+            prev = group_next
+            curr = group_prev.next
+
+            while curr != group_next:
+                next_node = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next_node
+
+            # connect reversed group
+            old_group_head = group_prev.next
+
+            group_prev.next = kth
+            group_prev = old_group_head
