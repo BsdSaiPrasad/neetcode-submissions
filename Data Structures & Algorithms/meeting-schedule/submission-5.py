5e8ed80e5049acd5fc1c3fb430c94f81e0996508
Sai Buthada <94958989+BsdSaiPrasad@users.noreply.github.com>
2026-09-02 19:37:05 -0400

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.py
@@ -0,0 +1,26 @@
+# TC: O(n log n) — because you sort by start time.
+# SC: O(1) auxiliary, excluding sorting internals.
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        intervals.sort(key = lambda x: x.start)
+
+        prev = intervals[0]
+
+        for curr in intervals[1:]:
+            if curr.start < prev.end:
+                return False
+            prev = curr
+        return True
+
