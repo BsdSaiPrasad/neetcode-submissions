f3899f8b8d494c3ca5b70e4d35f87501ab5d4fa4
Sai Buthada <94958989+BsdSaiPrasad@users.noreply.github.com>
2026-09-05 19:05:55 -0400

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,13 @@
+# TC: O(n) worst case
+# SC: O(1) auxiliary, excluding the output.
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits) - 1, -1, -1):
+
+            if digits[i] < 9:
+                digits[i] = digits[i] + 1
+                return digits
+            
+            digits[i] = 0
+
+        return [1] + digits
