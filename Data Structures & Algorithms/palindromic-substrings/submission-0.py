9190025d26b1ace071c663294f3755d5c103d183
Sai Buthada <94958989+BsdSaiPrasad@users.noreply.github.com>
2026-09-08 20:07:05 -0400

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+
+        for i in range(len(s)):
+            
+            #odd length
+            left = i
+            right = i
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count = count + 1
+
+                left = left - 1
+                right = right + 1
+            
+            left = i
+            right = i + 1
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                count = count + 1
+
+                left = left - 1
+                right = right + 1
+
+        return count
+                
